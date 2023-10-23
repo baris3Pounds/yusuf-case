49d4cab001d009a2d876156b77c46b39d25590c8
bkantarRDC <bkantar@idbs.com>
2023-10-23 14:37:37 +0300

    spring security fix

--- src/main/java/com/threepounds/caseproject/security/SecurityConfiguration.java
+++ src/main/java/com/threepounds/caseproject/security/SecurityConfiguration.java
@@ -52,7 +52,6 @@ public class SecurityConfiguration {
         auth
             .requestMatchers(mvcMatcherBuilder.pattern("/api/v1/auth/**")).permitAll()
             .requestMatchers(mvcMatcherBuilder.pattern("/api/v1/roles/**")).permitAll()
-            .requestMatchers(mvcMatcherBuilder.pattern("/api/v1/users/**")).hasRole("USER")
             //This line is optional in .authenticated() case as .anyRequest().authenticated()
             //would be applied for H2 path anyway
             .requestMatchers(PathRequest.toH2Console()).permitAll()
