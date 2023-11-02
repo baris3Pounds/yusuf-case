cc0d0b8c7b1280510475d2e037695bb67123f72f
bkantarRDC <bkantar@idbs.com>
2023-11-02 15:20:50 +0300

    redis config

--- src/main/java/com/threepounds/caseproject/redis/RedisConfiguration.java
+++ src/main/java/com/threepounds/caseproject/redis/RedisConfiguration.java
@@ -1,29 +1,24 @@
 package com.threepounds.caseproject.redis;
+
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
 @Configuration
 public class RedisConfiguration {
-        @Bean
-        public JedisConnectionFactory jedisConnectionFactory() {
-            RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
-            return new JedisConnectionFactory(redisStandaloneConfiguration);
-        }
 
-        @Bean
-        public <K,V> RedisTemplate<K, V> redisTemplate() {
-            RedisTemplate<K, V> template = new RedisTemplate<>();
-            template.setConnectionFactory(jedisConnectionFactory());
-            template.setKeySerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-            template.setHashKeySerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-            template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-            template.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-            return template;
-        }
-    }
+  @Bean
+  public RedisCacheConfiguration cacheConfiguration() {
+    return RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofMinutes(60))
+        .disableCachingNullValues()
+        .serializeValuesWith(
+            SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+  }
+
+}
 
 
